f818904305bd3b89e137fe45c1c6d9a9fc76792e
hYe807n <rlagkdus55@gmail.com>
2023-12-08 09:48:33 +0900

    fix: 뽑힌 동전들의 합계 금액 구하는 로직 수정

--- src/test/java/vendingmachine/model/MachineTest.java
+++ src/test/java/vendingmachine/model/MachineTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test;
 
 class MachineTest {
 
-    @DisplayName("랜덤하게 뽑힌 동전들의 합계가 투입 금액과 같은지 확인")
+    @DisplayName("랜덤하게 뽑힌 자판기 동전들의 합계가 투입 금액과 같은지 확인")
     @Test
     void checkSumCoins() {
-        int money = new Machine("500").getCoins().values().stream()
-            .mapToInt(value -> value).sum();
+        int money = new Machine("500").getCoins().entrySet()
+                .stream().mapToInt(coin -> coin.getValue() * coin.getKey().getAmount()).sum();
         Assertions.assertEquals(500, money);
     }
 }
