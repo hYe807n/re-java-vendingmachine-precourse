8895f81b96670ce5f155dadf8e2a581c01b4ce99
hYe807n <rlagkdus55@gmail.com>
2023-12-08 09:28:46 +0900

    test: 모든 상품의 개수가 0 개 라면 true 반환

--- src/test/java/vendingmachine/model/ProductsTest.java
+++ src/test/java/vendingmachine/model/ProductsTest.java
@@ -15,6 +15,12 @@ class ProductsTest {
     @DisplayName("투입 금액 500 원이 모든 상품 가격보다 낮다면 true 반환")
     @Test
     void isLessMinimumPrice() {
-        Assertions.assertTrue(new Products("[콜라-1000-1];[사이다,1500,2]").isLessMinimumPrice(100));
+        Assertions.assertTrue(new Products("[콜라,1000,1];[사이다,1500,2]").isLessMinimumPrice(100));
+    }
+
+    @DisplayName("모든 상품의 개수가 0 개 라면 true 반환")
+    @Test
+    void isAllSoldOut() {
+        Assertions.assertTrue(new Products("[콜라,1000,0];[사이다,1500,0]").isAllSoldOut());
     }
 }
