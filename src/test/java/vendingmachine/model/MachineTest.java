5e8bc8ed7e1f14e4df9ea3b6efd965a74bca06fc
hYe807n <rlagkdus55@gmail.com>
2023-12-08 09:46:39 +0900

    test: 랜덤하게 뽑힌 동전들의 합계가 투입 금액과 같은지 확인

---
+++ src/test/java/vendingmachine/model/MachineTest.java
@@ -0,0 +1,16 @@
+package vendingmachine.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MachineTest {
+
+    @DisplayName("랜덤하게 뽑힌 동전들의 합계가 투입 금액과 같은지 확인")
+    @Test
+    void checkSumCoins() {
+        int money = new Machine("500").getCoins().values().stream()
+            .mapToInt(value -> value).sum();
+        Assertions.assertEquals(500, money);
+    }
+}
