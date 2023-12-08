05937cdeae802933429647672555ceaee3546d0c
hYe807n <rlagkdus55@gmail.com>
2023-12-08 09:22:54 +0900

    test: 만약 없는 동전 값을 찾는다면 오류 발생

--- src/test/java/vendingmachine/enums/CoinTest.java
+++ src/test/java/vendingmachine/enums/CoinTest.java
@@ -11,4 +11,10 @@ class CoinTest {
     void findByAmount() {
         Assertions.assertEquals(Coin.findByAmount(500), Coin.COIN_500);
     }
+
+    @DisplayName("만약 없는 동전 값을 찾는다면 오류 발생")
+    @Test
+    void findByAmountNoneExist() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Coin.findByAmount(0));
+    }
 }
